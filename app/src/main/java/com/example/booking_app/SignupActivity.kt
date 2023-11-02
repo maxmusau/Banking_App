e8dc03c874609e0b17f50fe89aefb0839dd8468f
root <musaumaxwell@gmail.com>
2023-11-02 09:36:10 +0300

    first commit

---
+++ app/src/main/java/com/example/booking_app/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.booking_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
